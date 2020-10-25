17490f9a3e74b2d3ab7836e6ecae63281ffbb63a
jaehlee36 <jaehlee36@naver.com>
2020-10-25 19:44:46 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {Components} from 'react';
+import Subject from "./components/Subject"
+import TOC from "./components/TOC"
+import Content from "./components/Content"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Subject title="www" sub="World wide web"></Subject>
+        <TOC></TOC>
+        <Content title="HTML" description="HyperText"></Content>
     </div>
   );
 }
+
+// function Subject(props) {
+//   return (
+//       <header>
+//           <h1>{props.title}</h1>
+//           {props.sub}
+//       </header>
+//   )
+// }
+// function TOC() {
+//   return (
+//       <nav>
+//           <ul>
+//               <li><a href="1.html">HTML</a></li>
+//               <li><a href="2.html">CSS</a></li>
+//               <li><a href="3.html">JS</a></li>
+//           </ul>
+//       </nav>
+//   )
+// }
+// function Content(props) {
+//   return (
+//       <article>
+//           <h2>{props.title}</h2>
+//           {props.description}
+//       </article>
+//   )
+// }
+// class Content extends React.component {
+//   constructor(props) {
+//     super(props)
+//   }
+//   render() {
+//     return (
+//       <article>
+//           <h2>{this.props.title}</h2>
+//           {this.props.description}
+//       </article>
+//     );
+//   }
+// }
 
 export default App;
---
+++ src/components/TOC.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function TOC() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="1.html">HTML</a></li>
+                <li><a href="2.html">CSS</a></li>
+                <li><a href="3.html">JS</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default TOC;
